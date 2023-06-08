bd25336b2fe4405025b758882b8bdf62ad66d2ce
diksha-sontakke <84328313+diksha-sontakke@users.noreply.github.com>
2023-06-08 22:58:57 +0530

    one

---
+++ lib/constant/constant.dart
@@ -0,0 +1,11 @@
+
+import 'package:flutter/material.dart';
+
+
+
+const textColor = Color(0xFF1E1E1E);
+const backgroundColor = Color(0xFFFFFFFF);
+const subTextColor = Color(0xFFFFFFFF);
+const titleColor = Color(0xFF252525);
+
+const kDefaultPaddin = 20.0;
